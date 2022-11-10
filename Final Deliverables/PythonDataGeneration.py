e97d0861be7a67f54a8ef020257ac86f363f6793
t-manish <manishcs1011_cs@mepcoeng.ac.in>
2022-11-10 21:40:55 +0530

    PythonData

---
+++ Final Deliverables/PythonDataGeneration.py
@@ -0,0 +1,45 @@
+#Team ID: PNT2022TMID17812
+
+import time
+import sys
+import ibmiotf.application
+import ibmiotf.device
+import random
+
+# Provide your IBM Watson Device Credentials
+organization = "u7bs6g"
+deviceType = "GasSensor"
+deviceId = "121"
+authMethod = "token"
+authToken = "987654321"
+
+try:
+    deviceOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod,
+                     "auth-token": authToken}
+    deviceCli = ibmiotf.device.Client(deviceOptions)
+    deviceCli.connect()
+# ..............................................
+
+except ibmiotf.ConnectionException as e:
+    print("Caught exception connecting device: %s" % str(e))
+    sys.exit()
+
+while True:
+
+    temp = random.randint(0, 100)
+    hum = random.randint(0, 100)
+    gas = random.randint(0, 100)
+
+    mydata = {'temp': temp, 'hum': hum, 'gas': gas}
+
+    def on_publish():
+        print("Published Temperature = %s C" % temp, "Humidity = %s %%" % hum, "Gas Concentration = %s" % gas, "to IBM Watson")
+
+
+    success = deviceCli.publishEvent("IOTGasSensor", "json", mydata, qos=0, on_publish=on_publish)
+    if not success:
+        print("Not connected to IoTF")
+    time.sleep(2)
+
+# Disconnect the device and application from the cloud
+deviceCli.disconnect()
